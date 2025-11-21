935048dac16f71b92deee48d61b31a5a20394965
Tuan Truong <truongtuan97@yahoo.com>
2025-11-21 08:19:35 +0700

    implement

--- count_and_say.rb
+++ count_and_say.rb
@@ -1,12 +1,11 @@
 def count_and_say(n)
   result = "1"
 
-  (n - 1).times do
-    puts "Result: #{result.chars.chunk_while { |a, b| a == b }.inspect}"
+  (n - 1).times do    
     result = result.chars
                    .chunk_while { |a, b| a == b }
                    .map { |group| "#{group.size}#{group.first}" }
-                   .join
+                   .join                   
   end
 
   result
